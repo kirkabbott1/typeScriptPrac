1ed99dd2338848534c2e95fd61ba98db869d781e
Kirk Abbott <kirkabbott1@gmail.com>
2017-02-21 15:10:54 -0500

    add class ex and solution

---
+++ classEx.js
@@ -0,0 +1,120 @@
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+function Car(name) {
+    this.name = name;
+    this.acceleration = 0;
+    this.honk = function () {
+        console.log("Toooooooooot!");
+    };
+    this.accelerate = function (speed) {
+        this.acceleration = this.acceleration + speed;
+    };
+}
+var car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(10);
+console.log(car.acceleration);
+var baseObject = {
+    width: 0,
+    length: 0
+};
+var rectangle = Object.create(baseObject);
+rectangle.width = 5;
+rectangle.length = 2;
+rectangle.calcSize = function () {
+    return this.width * this.length;
+};
+console.log(rectangle.calcSize());
+var person = {
+    _firstName: ""
+};
+Object.defineProperty(person, "firstName", {
+    get: function () {
+        return this._firstName;
+    },
+    set: function (value) {
+        if (value.length > 3) {
+            this._firstName = value;
+        }
+        else {
+            this._firstName = "";
+        }
+    },
+    enumerable: true,
+    configurable: true
+});
+console.log(person.firstName);
+person.firstName = "Ma";
+console.log(person.firstName);
+person.firstName = "Maximilian";
+console.log(person.firstName);
+var Car = (function () {
+    function Car(name) {
+        this.acceleration = 0;
+        this.name = name;
+    }
+    Car.prototype.honk = function () {
+        console.log("Toooooooot!");
+    };
+    Car.prototype.accelerate = function (speed) {
+        this.acceleration = this.acceleration + speed;
+    };
+    return Car;
+}());
+var car = new Car("BMW");
+car.honk();
+console.log(car.acceleration);
+car.accelerate(20);
+console.log(car.acceleration);
+var BaseObject = (function () {
+    function BaseObject() {
+        this.width = 0;
+        this.length = 0;
+    }
+    return BaseObject;
+}());
+var Rectangle = (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Rectangle.prototype.calcSize = function () {
+        return this.width * this.length;
+    };
+    return Rectangle;
+}(BaseObject));
+var rectangle = new Rectangle();
+rectangle.width = 5;
+rectangle.length = 10;
+console.log(rectangle.calcSize());
+var Person = (function () {
+    function Person() {
+        this._firstName = "";
+    }
+    Object.defineProperty(Person.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            if (value.length > 3) {
+                this._firstName = value;
+            }
+            else {
+                this._firstName = "";
+            }
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Person;
+}());
+var person = new Person();
+console.log(person.firstName);
+person.firstName = "Ma";
+console.log(person.firstName);
+person.firstName = "Maximilian";
+console.log(person.firstName);
